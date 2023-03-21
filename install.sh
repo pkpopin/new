1b583a0450bf61d5b97429802a9592cd3688c515
EC2 Default User <ec2-user@ip-172-31-1-10.ap-south-1.compute.internal>
2023-03-21 18:16:28 +0000

    change

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ sudo systemctl start docker
 sudo systemctl enable docker
 
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-composesudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
